7297cdaa5db4d18296d2471448609ed48ff899b6
Michał Brodecki <m.brodecki@binarapps.com>
2014-06-20 14:49:15 +0200

    Barcodes presentation

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -1,6 +1,13 @@
+require 'barby'
+require 'barby/barcode/ean_13'
+require 'barby/outputter/ascii_outputter'
+require 'barby/outputter/html_outputter'
+
 class BarcodesController < ApplicationController
 
   def home
+    @barcode = Barby::EAN13.new('000000000000')
+    @barcode_for_html = Barby::HtmlOutputter.new(@barcode)
   end
 
 end
