6d06305c53eec8c5c27dea034f4183a0704d81c8
Michał Brodecki <m.brodecki@binarapps.com>
2014-06-20 14:54:59 +0200

    Code changed

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -6,7 +6,7 @@ require 'barby/outputter/html_outputter'
 class BarcodesController < ApplicationController
 
   def home
-    @barcode = Barby::EAN13.new('000000000000')
+    @barcode = Barby::EAN13.new('012345678912')
     @barcode_for_html = Barby::HtmlOutputter.new(@barcode)
   end
 
