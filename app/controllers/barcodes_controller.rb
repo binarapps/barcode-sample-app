2796f4731090bc95f29b403cd9db8b15ecab7293
Michał Brodecki <m.brodecki@binarapps.com>
2014-06-25 13:29:10 +0200

    Testing codes

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -7,7 +7,7 @@ require 'barby/barcode/code_25_interleaved'
 class BarcodesController < ApplicationController
 
   def home
-    @barcode = Barby::Code25Interleaved.new('627598 2 42 0007110202')
+    @barcode = Barby::Code25Interleaved.new('627598 2 42 0000000000')
     @barcode.include_checksum = true
     @barcode_for_html = Barby::HtmlOutputter.new(@barcode)
   end
