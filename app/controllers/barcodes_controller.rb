4c25a0b24dcee184b09727c16df9e9be13e00949
Michał Brodecki <m.brodecki@binarapps.com>
2014-06-25 13:27:06 +0200

    Interleaved code

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -2,11 +2,13 @@ require 'barby'
 require 'barby/barcode/ean_13'
 require 'barby/outputter/ascii_outputter'
 require 'barby/outputter/html_outputter'
+require 'barby/barcode/code_25_interleaved'
 
 class BarcodesController < ApplicationController
 
   def home
-    @barcode = Barby::EAN13.new('012345678912')
+    @barcode = Barby::Code25Interleaved.new('627598 2 42 0007110202')
+    @barcode.include_checksum = true
     @barcode_for_html = Barby::HtmlOutputter.new(@barcode)
   end
 
